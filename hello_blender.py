5af84427a5872e17644e6ce9fd693c2d509263a2
baohannguyen <bao.han.nguyen@hs-furtwangen.de>
2022-10-18 20:24:47 +0200

    Update hello_blender.py

--- hello_blender.py
+++ hello_blender.py
@@ -1,3 +1,9 @@
 import bpy
 
-obj = bpy.context.object
+bpy.context #erlaubt Zugriff auf den akt. Kontext, in dem sich der Benutzer befindet, z.B. Szene, Modus,...
+
+bpy.data # die .blend- Datei wird damit abgebildet
+
+bpy.ops # Operatoren = Transform, Position im Raum usw.
+
+bpy.types # Blenders interne Datentypen, z.B. Objekte, Materialien,...
